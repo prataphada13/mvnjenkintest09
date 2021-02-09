9e3ef6afb391252b9e8b0351dc7159bdb7bb1ecf
apple <pratap.pt49@gmail.com>
2021-02-09 11:11:19 +0530

    first commit-uploadng to git

---
+++ src/main/java/com/mb/MVNJenkinTest/App.java
@@ -0,0 +1,13 @@
+package com.mb.MVNJenkinTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
